e8d710cdcc8b4837ed6308787230c03d15d62ba8
Ben <yacoobbenyamain@gmail.com>
2023-05-07 22:51:14 -0400

    Block hits animation is done

--- Assets/Scripts/BlockHit.cs
+++ Assets/Scripts/BlockHit.cs
@@ -10,7 +10,7 @@ public class BlockHit : MonoBehaviour
     
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if (!animating && collision.gameObject.CompareTag("Player"))
+        if (!animating && maxHits != 0 && collision.gameObject.CompareTag("Player"))
         {
             // If Mario hit the block in the same direction
             if (collision.transform.DotTest(transform, Vector2.up))
@@ -40,6 +40,27 @@ public class BlockHit : MonoBehaviour
         Vector3 restingPosition = transform.localPosition;
         Vector3 animatedPosition = restingPosition + Vector3.up * 0.5f;
 
+        yield return Move(restingPosition, animatedPosition);
+        yield return Move(animatedPosition, restingPosition);
+
         animating = false;
+    }
+
+    private IEnumerator Move(Vector3 from, Vector3 to)
+    {
+        float elapsed = 0f;
+        float duration = 0.125f;
+
+        while (elapsed < duration)
+        {
+            float t = elapsed / duration;
+
+            transform.localPosition = Vector3.Lerp(from, to, t);
+            elapsed += Time.deltaTime;
+
+            yield return null;
+        }
+
+        transform.localPosition = to;
     }
 }
