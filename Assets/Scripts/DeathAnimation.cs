e27b5b0590549aba278bbf5768049642a54fc169
Ben <yacoobbenyamain@gmail.com>
2023-05-07 21:15:24 -0400

    DeathAnimation implemented if any of the characters die

---
+++ Assets/Scripts/DeathAnimation.cs
@@ -0,0 +1,77 @@
+using UnityEngine;
+using System.Collections;
+using System.Threading;
+
+public class DeathAnimation : MonoBehaviour
+{
+    public SpriteRenderer spriteRenderer;
+    public Sprite deadSprite;
+
+    private void Reset()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    private void OnEnable()
+    {
+        UpdateSprite();
+        DisablePhysics();
+        StartCoroutine(Animate());
+    }
+
+    private void UpdateSprite()
+    {
+        spriteRenderer.enabled = true;
+        spriteRenderer.sortingOrder = 10;   // On top of everything in the scene when enemies die (fall off the screen)
+
+        if (deadSprite != null)
+        {
+            spriteRenderer.sprite = deadSprite;
+        }
+    }
+
+    private void DisablePhysics()
+    {
+        Collider2D[] colliders = GetComponents<Collider2D>();
+
+        foreach (Collider2D collider in colliders)
+        {
+            collider.enabled = false;
+        }
+
+        // So physics engine will not affect that object
+        GetComponent<Rigidbody2D>().isKinematic = true;
+        PlayerMovement playerMovement = GetComponent<PlayerMovement>();
+        EntityMovement entityMovement = GetComponent<EntityMovement>();
+
+        if (playerMovement != null)
+        {
+            playerMovement.enabled = false;
+        }
+
+        if (entityMovement != null)
+        {
+            entityMovement.enabled = false;
+        }
+    }
+
+    // Needs to be a coroutine
+    private IEnumerator Animate()
+    {
+        float elapsed = 0f;
+        float duration = 3f;
+
+        float jumpVelocity = 10f;
+        float gravity = -36f;
+
+        Vector3 velocity = Vector3.up * jumpVelocity;
+
+        while (elapsed < duration)
+        {
+            transform.position += velocity * Time.deltaTime;    // Changing position over time
+            velocity.y += gravity * Time.deltaTime;
+            elapsed += Time.deltaTime;
+            yield return null;
+        }
+    }
+}
