a71714a4b14905bc9d2b1e28c237694fa456bcc4
Ben <yacoobbenyamain@gmail.com>
2023-05-08 20:25:48 -0400

    Pipe enter functionality is working. Going to work on pipe exit next

--- Assets/Scripts/Pipe.cs
+++ Assets/Scripts/Pipe.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using UnityEngine;
 
 public class Pipe : MonoBehaviour
@@ -22,6 +23,33 @@ public class Pipe : MonoBehaviour
 
     private IEnumerator Enter(Transform player)
     {
+        player.GetComponent<PlayerMovement>().enabled = false;
+        Vector3 enteredPosition = transform.position + enterDirection;
+        Vector3 enteredScale = Vector3.one * 0.5f;
 
+        yield return Move(player, enteredPosition, enteredScale);
+    }
+
+    private IEnumerator Move(Transform player, Vector3 endPosition, Vector3 endScale)
+    {
+        float elapsed = 0f;
+        float duration = 1f;
+
+        Vector3 startPosition = player.position;
+        Vector3 startScale = player.localScale;
+
+        while (elapsed < duration)
+        {
+            float t = elapsed / duration;
+
+            player.position = Vector3.Lerp(startPosition, endPosition, t);
+            player.localScale = Vector3.Lerp(startScale, endScale, t);
+            elapsed += Time.deltaTime;
+
+            yield return null;
+        }
+
+        player.position = endPosition;
+        player.localScale = endScale;
     }
 }
